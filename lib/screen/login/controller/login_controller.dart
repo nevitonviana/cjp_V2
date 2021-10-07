76276c510fc953ac8482ce95ff6621a7c4647151
neviton viana <nevitonviana1@hotmail.com>
2021-10-07 17:54:48 -0300

    login manager

---
+++ lib/screen/login/controller/login_controller.dart
@@ -0,0 +1,49 @@
+import 'package:mobx/mobx.dart';
+
+import '/components/extensions/extensions.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = _LoginControllerBase with _$LoginController;
+
+abstract class _LoginControllerBase with Store {
+  // text field
+  @observable
+  String? email;
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @computed
+  bool get emailValid => email != null && email!.isEmailValid();
+
+  String? get errorEmail =>
+      email == null || emailValid ? null : "E-mail Invalido";
+
+  //  password field
+  @observable
+  String? password;
+
+  @action
+  void setPassword(String value) => password = value;
+
+  @computed
+  bool get passwordValid => password != null && password!.length >= 6;
+
+  String? get errorPassword => password == null || passwordValid
+      ? null
+      : "Senha requer mais de 5 caracteres";
+
+  //visibility password
+  @observable
+  bool visibilityPassword = true;
+
+  @action
+  void setVisibilityPassword(bool value) => visibilityPassword = value;
+
+  //button
+  @computed
+  Function? get loginPressed => emailValid && passwordValid ? _login : null;
+
+  void _login() async {}
+}
