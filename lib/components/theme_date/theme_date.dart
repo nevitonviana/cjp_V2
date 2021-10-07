48c2a64bc93e91ed2b507ceb5fb014a30e7db38d
neviton viana <nevitonviana1@hotmail.com>
2021-10-07 14:51:41 -0300

    created file themeDate

--- lib/components/theme_date/theme_date.dart
+++ lib/components/theme_date/theme_date.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 final ThemeData theme = ThemeData(
-
+  scaffoldBackgroundColor: Colors.grey[200],
+  textTheme: const TextTheme(
+    bodyText1: TextStyle(color: Colors.black),
+    bodyText2: TextStyle(color: Colors.black),
+  ).apply(
+    bodyColor: Colors.black,
+    displayColor: Colors.black,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      elevation: MaterialStateProperty.all<double>(3),
+      backgroundColor: MaterialStateProperty.all(Colors.grey[400]),
+    ),
+  ),
 );
-
