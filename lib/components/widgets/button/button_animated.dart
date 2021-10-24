58efb706498196e152567d823270f25a975e7ae3
neviton viana <nevitonviana1@hotmail.com>
2021-10-23 21:16:16 -0300

    button animated

--- lib/components/widgets/button/button_animated.dart
+++ lib/components/widgets/button/button_animated.dart
@@ -1,4 +1,7 @@
+import 'package:cjp_v2/components/theme/font/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 class ButtonAnimated extends StatelessWidget {
@@ -17,30 +20,44 @@ class ButtonAnimated extends StatelessWidget {
   Widget build(BuildContext context) {
     return Observer(
       builder: (_) {
-        return AnimatedContainer(
-          curve: Curves.easeInCirc,
-          duration: const Duration(milliseconds: 600),
-          constraints: BoxConstraints.loose(
-            Size(width, 40),
-          ),
-          height: controller.pressed == null ? 0 : 40,
-          width: width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            border: const Border.fromBorderSide(
-              BorderSide(color: Colors.grey),
+        return GestureDetector(
+          onTap: controller.pressed,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100)),
+            elevation: 3,
+            child: AnimatedContainer(
+              alignment: Alignment.center,
+              curve: Curves.easeInCirc,
+              duration: const Duration(milliseconds: 600),
+              constraints: BoxConstraints.loose(
+                Size(width, 40),
+              ),
+              height: controller.pressed == null ? 0 : 55,
+              width: controller.loading ? 50 : width,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.circular(controller.loading ? 100 : 6),
+                border: const Border.fromBorderSide(
+                  BorderSide(color: Colors.grey),
+                ),
+                color: Colors.grey[400],
+              ),
+              child: AnimatedCrossFade(
+                firstChild: Text(
+                  name,
+                  style: Fonts.fontsRoboto,
+                ),
+                secondChild: const CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
+                crossFadeState: controller.loading
+                    ? CrossFadeState.showSecond
+                    : CrossFadeState.showFirst,
+                duration: const Duration(milliseconds: 600),
+              ),
             ),
-          ),
-          child: ElevatedButton(
-            onPressed: controller.pressed,
-            child: controller?.loading
-                ? const CircularProgressIndicator(
-                    color: Colors.blue,
-                  )
-                : Text(
-                    name,
-                    style: const TextStyle(color: Colors.black),
-                  ),
           ),
         );
       },
